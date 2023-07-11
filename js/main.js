3fc15d5bbf737125af3b2b3eab5c2f8c34735ad9
srwesteban <sr.w@hotmail.com>
2023-07-11 03:50:01 -0500

    fin

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const images = [
+    'img/background1.jpg',
+    'img/background2.jpg',
+    'img/background3.jpg',
+  ];
+  
+  // App state
+  let index = 0;
+  const max = images.length;
+  
+  // Get the DOM elements
+  const containerElement = document.querySelector('.container');
+  const prevSlideButton = document.querySelector('.prevSlide');
+  const nextSlideButton = document.querySelector('.nextSlide');
+  
+  // Listen for arrow click events
+  prevSlideButton.addEventListener('click', function () {
+    index--;
+    setImageIndex();
+    changeBackgroundImage(images[index], containerElement);
+  });
+  
+  nextSlideButton.addEventListener('click', function () {
+    index++;
+    setImageIndex();
+    changeBackgroundImage(images[index], containerElement);
+  });
+  
+  // Utility function
+  function setImageIndex() {
+    if (index < 0) index = max - 1;
+    if (index === max) index = 0;
+  }
+  
+  function changeBackgroundImage(backgroundImage, element) {
+    element.style.backgroundImage = `url(${backgroundImage})`;
+  }
+  
